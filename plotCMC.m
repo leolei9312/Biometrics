327b8662ab44bff08c6034cb7dcb1317483d75ac
Menglei Lei <mengleilei2018@u.northwestern.edu>
2017-10-26 21:07:34 -0500

    almost finish

---
+++ plotCMC.m
@@ -0,0 +1,60 @@
+prob = load('VerificationProb1.mat');
+gallery = load('Gallery.mat');
+
+[rowProb, colProb] = size(prob.veri);
+[rowGal, colGal] = size(gallery.matrix);
+maps = {};
+
+for i = 1 : rowProb
+    disp(i);
+    map = {};
+    name = prob.veri{i, 1};
+    eye = prob.veri{i, 4};
+    temp = prob.veri{i, 2};
+    mask = prob.veri{i, 3};
+    for j = 1 : rowGal
+        nameGal = gallery.matrix{j, 1};
+        eyeGal = gallery.matrix{j, 4};
+        tempGal = gallery.matrix{j, 2};
+        maskGal = gallery.matrix{j, 3};
+        hd = gethammingdistance(temp, mask, tempGal, maskGal, 1);
+        if isnan(hd)
+            continue;
+        end
+        map = [map; {nameGal, hd}];
+    end
+    sorted = sortrows(map, [2]);
+    maps = [maps; {name, sorted}];
+end
+save sortedProb1CMC.mat maps;
+
+maps = load('sortedProb1CMC.mat');
+maps = maps.maps;
+
+[rowmap, colmap] = size(maps);
+res = [];
+for k = 1 : rowGal
+    count = 0;
+    for i = 1 : rowmap
+        name = maps{i, 1};
+        sortedMap = maps{i, 2};
+        [row, col] = size(sortedMap);
+        if k >= row
+           count = count + 1;
+           continue;
+        end
+        for j = 1 : k
+            if strcmpi(sortedMap{j, 1}, name)
+               count = count + 1;
+               break;
+            end
+        end
+    end
+    count = count / rowmap;
+    res = [res, count];
+end
+x = 1 : 1 : rowGal;
+y = res;
+plot(x, y);
+xlabel('Rank');
+ylabel('Recognition Rate');
